93b446fe1782ed0bd158e236ad1ae829127be767
ParkSeungWoo <tmd24710@naver.com>
2021-03-28 17:49:30 +0900

    add deeplearning project

---
+++ DeepLearning/train.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+print('hello CNN!')
+
+# data preprocessing
+def preprocess():
+    print('preprocess')
+    return
+
+
+def model():
+    return
+
